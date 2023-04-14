17bb40291506e9bca40b15cdefb539a808ebda13
oohkumar123 <115493729+oohkumar123@users.noreply.github.com>
2023-04-14 09:53:51 -0700

    changed remote path 2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'https://github.com/oohkumar123/WeatherDashboardVueJs/'
+  publicPath: 'WeatherDashboardVueJs/'
 })
